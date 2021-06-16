ff7b66a7a2073ef053d18f95a9a6753aaf137ff2
aaronfeingold <ajfeingold88@gmail.com>
2021-06-16 16:43:19 -0500

    type http for cors to work...

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://localhost:8080",
+  baseURL: "http://localhost:8080/api/artist-events",
   headers: {
     "Content-type": "application/json"
   }
