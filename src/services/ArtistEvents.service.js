a0606afae7fc74e4975e0004d4ccaf30335fee6f
aaronfeingold <ajfeingold88@gmail.com>
2021-06-17 17:40:41 -0500

    set up correct API endpoint

--- src/services/ArtistEvents.service.js
+++ src/services/ArtistEvents.service.js
@@ -1,11 +1,6 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios'
 
-// const api = axios.create({
-//   baseURL: "https://w8ywpmy259.execute-api.us-east-1.amazonaws.com/default/oz-re-wire",
-//   headers: {"Access-Control-Allow-Origin": "*"}
-// })
-
 export const fetchArtistEvents = createAsyncThunk(
   "aes/fetchArtistEvents",
   // get req to AWS API gateway, calls Lambda scrapper python function
