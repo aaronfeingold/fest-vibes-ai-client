78abbe80bf71cf0db869462b0bb7a688fd6fe6bb
aaronfeingold <ajfeingold88@gmail.com>
2021-06-17 00:28:32 -0500

    revert

--- src/axiosAPI.js
+++ src/axiosAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://www.wwoz.org/calendar/livewire-music",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://www.wwoz.org/calendar/livewire-music",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
