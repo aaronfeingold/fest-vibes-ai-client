53fbe3965af8d0727363a936ee6c605a43709555
aaronfeingold <ajfeingold88@gmail.com>
2021-06-17 00:26:28 -0500

    appease netlify gods

--- src/axiosAPI.js
+++ src/axiosAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://www.wwoz.org/calendar/livewire-music",
+  baseURL: "https://www.wwoz.org/calendar/livewire-music",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
