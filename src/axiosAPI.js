55ab31dade88ee4cb381938d10ff4166ebf55f05
aaronfeingold <ajfeingold88@gmail.com>
2021-06-16 19:22:53 -0500

    switching up nods js to serverside work

--- src/axiosAPI.js
+++ src/axiosAPI.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: "http://localhost:8080/api/artist-events",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
   }
 });
