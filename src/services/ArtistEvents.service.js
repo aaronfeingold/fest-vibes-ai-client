272c7ef9be0940b658045122c9886059193142af
Aaron Feingold <65971210+aaronfeingold@users.noreply.github.com>
2021-06-20 16:49:21 -0500

    Update ArtistEvents.service.js

--- src/services/ArtistEvents.service.js
+++ src/services/ArtistEvents.service.js
@@ -1,22 +1,13 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
-<<<<<<< HEAD
-import axios from 'axios'
-=======
 import { axiosAPI } from '../axiosAPI'
->>>>>>> refactor
 
 export const fetchArtistEvents = createAsyncThunk(
   "aes/fetchArtistEvents",
   // get req to AWS API gateway, calls Lambda scrapper python function
   async () => {
-<<<<<<< HEAD
-    const data = await axios("https://w8ywpmy259.execute-api.us-east-1.amazonaws.com/default/oz-re-wire");
-    return data
-=======
     const response = await axiosAPI.get("/");
     const payload = response.data
     return payload
->>>>>>> refactor
   }
 )
 
