900ce89dd0bba48956feae041b3ffbbfeac6ba7a
aaronfeingold <ajfeingold88@gmail.com>
2021-06-20 17:22:39 -0500

    baseURL change

--- src/axiosAPI.js
+++ src/axiosAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "https://w8ywpmy259.execute-api.us-east-1.amazonaws.com/beta/oz-re-wire"
+const BASE_URL = "https://w8ywpmy259.execute-api.us-east-1.amazonaws.com/default/oz-re-wire"
 
 export const axiosAPI = axios.create({
     baseURL: `${BASE_URL}`,
