659856bacc1d209394faa80ec69ebe38e5af4ca2
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 17:48:47 -0400

    refactor the Searcher card with some CSS

--- src/components/cards/Searcher.card.js
+++ src/components/cards/Searcher.card.js
@@ -1,36 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setFilterStatus, updateQuery } from "../../slices/ArtistEvents.slice";
+import styles from "./Searcher.card.module.css";
 
 const Searcher = () => {
-  let dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState(""); // State to manage the search term
+  const [debouncedTerm, setDebouncedTerm] = useState(searchTerm);
 
-  let handleQuery = (e) => {
-    let query = e.target.value;
-    if (query !== "") {
+  // Update debounced term after a delay
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedTerm(searchTerm);
+    }, 300); // 300ms debounce delay
+
+    // Cleanup the timeout on component unmount or on searchTerm change
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchTerm]);
+
+  // Update Redux store based on debounced term
+  useEffect(() => {
+    if (debouncedTerm) {
       dispatch(setFilterStatus(true));
-      dispatch(updateQuery(e.target.value));
+      dispatch(updateQuery(debouncedTerm));
     } else {
       dispatch(setFilterStatus(false));
       dispatch(updateQuery(""));
     }
+  }, [debouncedTerm, dispatch]);
+
+  // Handle input change
+  const handleQuery = (e) => {
+    setSearchTerm(e.target.value);
   };
 
   return (
     <div className="container-sm">
       <div className="row justify-content-center">
-        <div className="card" style={{ width: "30rem" }}>
-          <div className="card-body">
-            <input
-              type="query"
-              className="form-control"
-              id="floatingInput"
-              placeholder="The Best Band Ever"
-              onChange={handleQuery}
-            />
-            <label htmlFor="floatingInput">Search by artist name</label>
-          </div>
-          <br />
+        <div
+          className={`input-group ${styles.inputGroup}`}
+          style={{ width: "30rem" }}
+        >
+          <input
+            type="text"
+            className={`form-control ${styles.formControl}`}
+            placeholder="Search by artist name"
+            aria-label="Search by artist name"
+            onChange={handleQuery}
+            value={searchTerm}
+          />
+          <button
+            className={`btn btn-outline-secondary ${styles.btnOutlineSecondary}`}
+            type="button"
+          >
+            Search
+          </button>
         </div>
       </div>
     </div>
