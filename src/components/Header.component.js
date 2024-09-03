7407a3dda5c72f51f604e58b6d05c91f339624a8
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 16:50:16 -0400

    abstract the header

---
+++ src/components/Header.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <div className="row justify-content-center">
+    <h3 className="text-center">This is your OZ Live-re-Wire</h3>
+  </div>
+);
+
+export default Header;
