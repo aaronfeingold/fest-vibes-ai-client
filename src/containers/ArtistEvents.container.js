e95c41db5c624797b69e877710215cbbf35d7fbc
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 17:49:35 -0400

    remove search from artist events container in favor of being in the nav bar

--- src/containers/ArtistEvents.container.js
+++ src/containers/ArtistEvents.container.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ArtistEventsList from "../components/lists/ArtistEvents.list";
-import Searcher from "../components/cards/Searcher.card";
 
 const ArtistEvents = ({ filterStatus, query, apiStatus }) => (
   <div className="container-lg">
-    <Searcher filterStatus={filterStatus} />
     <br />
     <ArtistEventsList
       apiStatus={apiStatus}
