62ab52e3f842b4deb8aee5b32ec181fc58a27161
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 17:50:10 -0400

    use the date utilities in the header

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { formatDate, getOrdinalSuffix } from "../utils/formatDate";
 
-const Header = () => (
-  <div className="row justify-content-center">
-    <h3 className="text-center">This is your OZ Live-re-Wire</h3>
-  </div>
-);
+const Header = () => {
+  const today = new Date();
+  const formattedDate = formatDate(today);
+  const day = today.getDate();
+  const ordinalSuffix = getOrdinalSuffix(day);
+
+  // Split the formatted date to insert the ordinal suffix
+  const [monthAndDay, year] = formattedDate.split(",");
+  const [month, dayWithoutSuffix] = monthAndDay.split(" ");
+  return (
+    <div className="row justify-content-center">
+      <h3 className="text-center">
+        This is your AJF Live-re-Wire Music Calender for{" "}
+        {`${month} ${dayWithoutSuffix}${ordinalSuffix},${year}`}
+      </h3>
+    </div>
+  );
+};
 
 export default Header;
