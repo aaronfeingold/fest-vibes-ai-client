6fdd8a799495a69eee3309cf66235f5517fd7d17
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 16:51:06 -0400

    remove snake case from js file;

--- src/components/cards/ArtistEvent.card.js
+++ src/components/cards/ArtistEvent.card.js
@@ -1,32 +1,39 @@
 import React from 'react'
 
-const ArtistEvent = ({ae}) => {
-  let artist_name;
-  let event_href;
+const ArtistEvent = ({ ae }) => {
+  let artistName;
+  let eventHref;
 
-  for (var key in ae) {
-    if (ae.hasOwnProperty(key)){
-      event_href = ae[key]
-      artist_name = key
+  for (let key in ae) {
+    if (ae.hasOwnProperty(key)) {
+      eventHref = ae[key];
+      artistName = key;
     }
   }
 
-  return(
+  return (
     <div className="row justify-content-center">
-        <div className="card" id={artist_name} style={{ width: '30rem' }}>
-          <div className="card-body">
-            <ul className="card-info">
-              <li className="card-info artist-name">
-                {artist_name}
-              </li>
-              <li className="card-info event-href">
-                <button className="btn btn-primary" target="_blank" rel="noopener noreferrer" onClick={()=> window.open(`https://www.wwoz.org${event_href}`, "_blank")}>Details Here</button>
-              </li>
-            </ul>
-          </div>
+      <div className="card" id={artistName} style={{ width: "30rem" }}>
+        <div className="card-body">
+          <ul className="card-info">
+            <li className="card-info artist-name">{artistName}</li>
+            <li className="card-info event-href">
+              <button
+                className="btn btn-primary"
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() =>
+                  window.open(`https://www.wwoz.org${eventHref}`, "_blank")
+                }
+              >
+                Details Here
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default ArtistEvent;
