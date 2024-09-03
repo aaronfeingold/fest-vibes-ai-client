d32af18149d94fc3230e7b0b8601c34549ce0c13
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 17:49:53 -0400

    not much to change to home page but a some fomratting

--- src/containers/Home.container.js
+++ src/containers/Home.container.js
@@ -1,22 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { fetchArtistEvents } from "../services/ArtistEvents.service";
 import ArtistEvents from "../containers/ArtistEvents.container";
 import Spinner from "../components/Spinner.component";
 import Header from "../components/Header.component";
+import Navbar from "../components/Navbar.component";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const aesState = useSelector((state) => state.aes, shallowEqual);
-
-  const { filterStatus, query, apiStatus } = aesState;
+  const { filterStatus, query, apiStatus } = useSelector(
+    (state) => state.aes,
+    shallowEqual
+  );
+  const scrollContainerRef = useRef(null);
+  const [showNavbar, setShowNavbar] = useState(false);
 
   useEffect(() => {
     dispatch(fetchArtistEvents());
   }, [dispatch]);
 
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        setShowNavbar(true);
+      } else {
+        setShowNavbar(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const handleScroll = (event) => {
+    if (scrollContainerRef.current) {
+      const nextSection = scrollContainerRef.current.nextElementSibling;
+      if (nextSection) {
+        nextSection.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
   return (
-    <div className="container-lg">
+    <div
+      className="container-lg"
+      onWheel={handleScroll}
+      ref={scrollContainerRef}
+    >
+      <Navbar filterStatus={filterStatus} />
       <Header />
       {apiStatus === "loading" ? (
         <Spinner />
