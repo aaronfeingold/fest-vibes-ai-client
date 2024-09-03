1731d1fe7eacda1be2bb64f2012be2731452512f
aaronfeingold <ajfeingold88@gmail.com>
2024-09-03 16:16:12 -0400

    searcher should not yet be a tsx file.

--- src/components/cards/Searcher.card.js
+++ src/components/cards/Searcher.card.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { setFilterStatus, updateQuery } from '../../slices/ArtistEvents.slice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setFilterStatus, updateQuery } from "../../slices/ArtistEvents.slice";
 
 const Searcher = () => {
   let dispatch = useDispatch();
 
-  let handleQuery = (e: { target: { value: string } }) => {
-    let query = e.target.value
-    if (query !== ""){
+  let handleQuery = (e) => {
+    let query = e.target.value;
+    if (query !== "") {
       dispatch(setFilterStatus(true));
       dispatch(updateQuery(e.target.value));
     } else {
@@ -19,12 +19,18 @@ const Searcher = () => {
   return (
     <div className="container-sm">
       <div className="row justify-content-center">
-        <div className="card" style={{width: '30rem'}}>
+        <div className="card" style={{ width: "30rem" }}>
           <div className="card-body">
-            <input type="query" className="form-control" id="floatingInput" placeholder="The Best Band Ever" onChange={handleQuery}/>
+            <input
+              type="query"
+              className="form-control"
+              id="floatingInput"
+              placeholder="The Best Band Ever"
+              onChange={handleQuery}
+            />
             <label htmlFor="floatingInput">Search by artist name</label>
           </div>
-          <br/>
+          <br />
         </div>
       </div>
     </div>
