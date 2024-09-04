2a3f2de5675d7f28c8aa369b5c364f4b04516172
aaronfeingold <ajfeingold88@gmail.com>
2024-09-04 15:50:32 -0400

    home now links to the home container

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -22,7 +22,7 @@ const Navbar = ({ filterStatus }) => (
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
+            <a className="nav-link active" aria-current="page" href="#home">
               Home
             </a>
           </li>
