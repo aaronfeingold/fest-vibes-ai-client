7246329d5a33ee587a509f21ecbcb6710ece4fe2
aaronfeingold <ajfeingold88@gmail.com>
2024-09-04 18:04:24 -0400

    some styles changes to the artist events container

--- src/containers/ArtistEvents.container.js
+++ src/containers/ArtistEvents.container.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ArtistEventsList from "../components/lists/ArtistEvents.list";
+import styles from "./ArtistEvents.container.module.css";
 
 const ArtistEvents = ({ filterStatus, query, apiStatus }) => (
-  <div className="container-lg">
+  <div className={`container-lg ${styles.artistEventsContainer}`}>
     <br />
     <ArtistEventsList
       apiStatus={apiStatus}
