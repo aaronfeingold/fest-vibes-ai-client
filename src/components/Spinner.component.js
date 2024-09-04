f61356df564567e969abae74ea2fb63ba679e9ea
aaronfeingold <ajfeingold88@gmail.com>
2024-09-04 15:51:21 -0400

    spinner has now concerned itself with the apiStatus

--- src/components/Spinner.component.js
+++ src/components/Spinner.component.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { useSelector, shallowEqual } from "react-redux";
 
-const Spinner = () => (
-  <div className="text-center">
-    <div
-      className="spinner-border"
-      role="status"
-      style={{ marginTop: 50, marginBottom: 50 }}
-    >
-      <span className="sr-only"></span>
-    </div>
-  </div>
-);
+const Spinner = () => {
+  const { apiStatus } = useSelector((state) => state.aes, shallowEqual);
+  if (apiStatus === "loading") {
+    return (
+      <div className="text-center">
+        <div
+          className="spinner-border"
+          role="status"
+          style={{ marginTop: 50, marginBottom: 50 }}
+        >
+          <span className="sr-only"></span>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Spinner;
