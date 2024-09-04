76b9d012945c953dc21b0398045b3b1d66df81e3
aaronfeingold <ajfeingold88@gmail.com>
2024-09-04 15:49:49 -0400

    hero section with css

---
+++ src/containers/HeroSection.container.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../components/Header.component";
+import About from "../components/About.component";
+import Spinner from "../components/Spinner.component";
+import styles from "./HeroSection.container.module.css";
+
+const HeroSection = () => {
+  return (
+    <div className={styles.heroSection}>
+      <div className={styles.parallax}>
+        <div className="container text-center text-white">
+          <h1 className="display-4 fw-bold">
+            <Header />
+          </h1>
+          <div className="col-lg-6 mx-auto">
+            <About />
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
