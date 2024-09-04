583e2d5ad3db97f7be92d37abbdc2087525f8fca
aaronfeingold <ajfeingold88@gmail.com>
2024-09-04 17:22:49 -0400

    a developement more to parse a json file

--- src/services/ArtistEvents.service.js
+++ src/services/ArtistEvents.service.js
@@ -5,8 +5,21 @@ export const fetchArtistEvents = createAsyncThunk(
   "aes/fetchArtistEvents",
   // get req to AWS API gateway, calls Lambda scrapper python function
   async () => {
+    if (process.env.NODE_ENV === "development") {
+      try {
+        const response = await fetch("/data.json"); // Fetch the local JSON file
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const json = await response.json(); // Parse the response body as JSON
+        return json;
+      } catch (error) {
+        console.error("Error fetching local data.json:", error);
+        throw error;
+      }
+    }
     const response = await axiosAPI.get("/");
-    const payload = response.data
-    return payload
+    const payload = response.data;
+    return payload;
   }
 )
