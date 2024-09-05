c2304e2cf6ca786330335b9b7e553f8652302325
aaronfeingold <ajfeingold88@gmail.com>
2024-09-05 12:45:10 -0400

    spinner now just uses the audio spinner from the react loader spinner library

--- src/components/Spinner.component.js
+++ src/components/Spinner.component.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { useSelector, shallowEqual } from "react-redux";
 import { Audio } from "react-loader-spinner";
 
-const Spinner = () => {
-  const { apiStatus } = useSelector((state) => state.aes, shallowEqual);
-  if (apiStatus === "loading") {
-    return (
-      <Audio
-        height="100"
-        width="100"
-        color="#4fa94d"
-        ariaLabel="audio-loading"
-        wrapperStyle={{}}
-        wrapperClass="wrapper-class"
-        visible={true}
-      />
-    );
-  }
-};
+const Spinner = () => (
+  <Audio
+    height="100"
+    width="100"
+    color="#4fa94d"
+    ariaLabel="audio-loading"
+    wrapperStyle={{}}
+    wrapperClass="wrapper-class"
+    visible={true}
+  />
+);
 
 export default Spinner;
