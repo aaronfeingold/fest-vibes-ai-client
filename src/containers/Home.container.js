431f903f879bdd46cc7ea17e01a4da82a00fc555
aaronfeingold <ajfeingold88@gmail.com>
2024-09-05 15:33:24 -0400

    a scroll to events function...that doesnt seem to do the scroll...

--- src/containers/Home.container.js
+++ src/containers/Home.container.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { fetchArtistEvents } from "../services/ArtistEvents.service";
 import ArtistEvents from "./ArtistEvents.container";
@@ -11,20 +11,33 @@ const Home = () => {
     (state) => state.aes,
     shallowEqual
   );
+  const artistEventsRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchArtistEvents());
   }, [dispatch]);
 
+  // Function to scroll to the artist events section
+  const scrollToEvents = () => {
+    if (artistEventsRef.current) {
+      console.log("barfoo");
+      artistEventsRef.current.scrollIntoView({
+        behavior: "smooth", // Smooth scroll effect
+      });
+    }
+  };
+
   return (
     <>
       <Navbar filterStatus={filterStatus} />
-      <HeroSection />
-      <ArtistEvents
-        filterStatus={filterStatus}
-        query={query}
-        apiStatus={query}
-      />
+      <HeroSection scrollToEvents={scrollToEvents} />
+      <div ref={artistEventsRef}>
+        <ArtistEvents
+          filterStatus={filterStatus}
+          query={query}
+          apiStatus={query}
+        />
+      </div>
     </>
   );
 };
