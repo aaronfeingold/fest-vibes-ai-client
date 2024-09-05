dce5ca8830b86f2eb323fd3ab70b5ef63bbc163a
aaronfeingold <ajfeingold88@gmail.com>
2024-09-05 12:41:51 -0400

    logic to handle having the user experience the loading gif for a little extra time just for fun

--- src/containers/HeroSection.container.js
+++ src/containers/HeroSection.container.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useMemo, useEffect, useState } from "react";
+import { useSelector, shallowEqual } from "react-redux";
 import Header from "../components/Header.component";
-import About from "../components/About.component";
 import Spinner from "../components/Spinner.component";
 import styles from "./HeroSection.container.module.css";
 
+const MINIMUM_LOADING_TIME = 3000;
+
 const HeroSection = () => {
+  const { apiStatus } = useSelector((state) => state.aes, shallowEqual);
+  const [spinnerVisible, setSpinnerVisible] = useState(false);
+  const [spinnerTimeout, setSpinnerTimeout] = useState(null);
+  // Effect to manage spinner visibility based on API status
+  useEffect(() => {
+    if (apiStatus === "loading") {
+      setSpinnerVisible(true);
+      // Clear any existing timeout
+      if (spinnerTimeout) {
+        clearTimeout(spinnerTimeout);
+      }
+      // Set a new timeout to hide the spinner after the minimum duration
+      const timeoutId = setTimeout(() => {
+        setSpinnerVisible(false);
+      }, MINIMUM_LOADING_TIME);
+      setSpinnerTimeout(timeoutId);
+    } else {
+      // If API status is not loading, hide the spinner immediately
+      if (spinnerTimeout) {
+        clearTimeout(spinnerTimeout);
+        setSpinnerVisible(false);
+      }
+    }
+  }, [apiStatus, spinnerTimeout]);
+
+  const header = useMemo(() => {
+    return (
+      <h1 className="display-4 fw-bold">
+        {spinnerVisible ? <Spinner /> : <Header />}
+      </h1>
+    );
+  }, [spinnerVisible]);
+
   return (
     <div className={styles.heroSection}>
       <div className={styles.parallax}>
-        <div className="container text-center text-black">
-          <h1 className="display-4 fw-bold">
-            <Header />
-          </h1>
-          <div className="col-lg-6 mx-auto">
-            <About />
-            <Spinner />
-          </div>
-        </div>
+        <div className="container text-center text-black">{header}</div>
       </div>
     </div>
   );
