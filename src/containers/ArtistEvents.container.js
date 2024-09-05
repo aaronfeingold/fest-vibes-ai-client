eefeca3241541c1a1fa68e1d3f708ed1a25e8192
aaronfeingold <ajfeingold88@gmail.com>
2024-09-05 16:53:25 -0400

    add an id to the artist events container

--- src/containers/ArtistEvents.container.js
+++ src/containers/ArtistEvents.container.js
@@ -3,7 +3,10 @@ import ArtistEventsList from "../components/lists/ArtistEvents.list";
 import styles from "./ArtistEvents.container.module.css";
 
 const ArtistEvents = ({ filterStatus, query, apiStatus }) => (
-  <div className={`container-lg ${styles.artistEventsContainer}`}>
+  <div
+    id="artistEvents"
+    className={`container-lg ${styles.artistEventsContainer}`}
+  >
     <ArtistEventsList
       apiStatus={apiStatus}
       filterStatus={filterStatus}
