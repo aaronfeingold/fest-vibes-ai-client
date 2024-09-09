7409cb9694d47b83ee7f376e85193a85ac185e71
aaronfeingold <ajfeingold88@gmail.com>
2024-09-09 13:27:27 -0400

    refactored state waters to the parent to passdown the spinner visibility, since really we were already watching the aes state in home already

--- src/containers/HeroSection.container.js
+++ src/containers/HeroSection.container.js
@@ -1,39 +1,10 @@
-import React, { useMemo, useEffect, useState } from "react";
-import { useSelector, shallowEqual } from "react-redux";
+import React, { useMemo } from "react";
 import Header from "../components/Header.component";
 import Spinner from "../components/Spinner.component";
 import styles from "./HeroSection.container.module.css";
-import useScroll from "../hooks/useScroll";
 
-const MINIMUM_LOADING_TIME = 3000;
-
-const HeroSection = () => {
-  const { apiStatus } = useSelector((state) => state.aes, shallowEqual);
-  const [spinnerVisible, setSpinnerVisible] = useState(true);
-  const { scrollToEvents } = useScroll();
-  const startTime = useMemo(() => Date.now(), []);
-
-  useEffect(() => {
-    let timeoutId;
-
-    if (apiStatus === "succeeded") {
-      // Wait for the minimum loading time before hiding the spinner
-      const elapsedTime = Date.now() - startTime;
-      const remainingTime = Math.max(0, MINIMUM_LOADING_TIME - elapsedTime);
-
-      timeoutId = setTimeout(() => {
-        setSpinnerVisible(false);
-      }, remainingTime);
-    }
-
-    // Cleanup function to clear the timeout if component unmounts
-    return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-    };
-  }, [apiStatus]);
-
+const HeroSection = ({ spinnerVisible, scrollToEvents }) => {
+  // UX: Hide the spinner after a minimum loading time
   const header = useMemo(() => {
     return (
       <h1 className="display-4 fw-bold">
@@ -42,10 +13,10 @@ const HeroSection = () => {
     );
   }, [spinnerVisible]);
 
-  // Handle the mouse wheel event
+  // UX: Handle the mouse wheel
   const handleWheel = (event) => {
     if (event.deltaY > 0) {
-      scrollToEvents(); // Trigger scroll to the ArtistEvents section when the user scrolls down
+      scrollToEvents();
     }
   };
 
