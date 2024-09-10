bfd9d4a8a2afdcac590f00bf2f980a912eb05bd6
aaronfeingold <ajfeingold88@gmail.com>
2024-09-10 16:56:08 -0400

    if spinner is visible, do not render the the list

--- src/containers/ArtistEvents.container.js
+++ src/containers/ArtistEvents.container.js
@@ -2,17 +2,20 @@ import React from "react";
 import ArtistEventsList from "../components/lists/ArtistEvents.list";
 import styles from "./ArtistEvents.container.module.css";
 
-const ArtistEvents = ({ filterStatus, query, apiStatus }) => (
-  <div
-    id="artistEvents"
-    className={`container-lg ${styles.artistEventsContainer}`}
-  >
-    <ArtistEventsList
-      apiStatus={apiStatus}
-      filterStatus={filterStatus}
-      query={query}
-    />
-  </div>
-);
+const ArtistEvents = ({ filterStatus, query, apiStatus, spinnerVisible }) =>
+  !spinnerVisible ? (
+    <div
+      id="artistEvents"
+      className={`container-lg ${styles.artistEventsContainer}`}
+    >
+      <ArtistEventsList
+        apiStatus={apiStatus}
+        filterStatus={filterStatus}
+        query={query}
+      />
+    </div>
+  ) : (
+    <></>
+  );
 
 export default ArtistEvents;
