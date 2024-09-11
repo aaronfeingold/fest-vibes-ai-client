ba2b81f34373ca1b56cbe3469b79c7ab84481b77
aaronfeingold <ajfeingold88@gmail.com>
2024-09-11 19:28:22 -0400

    use the new layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import Layout from "./containers/Layout";
 import Home from "./containers/Home.container";
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
   </Router>
 );
 
