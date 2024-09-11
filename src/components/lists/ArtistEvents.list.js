689b0485bc174cc35ba8ea6a7bf17a00760cb22a
aaronfeingold <ajfeingold88@gmail.com>
2024-09-11 19:30:55 -0400

    extract logic from the list to its component

--- src/components/lists/ArtistEvents.list.js
+++ src/components/lists/ArtistEvents.list.js
@@ -1,75 +1,18 @@
-import React, { useMemo, useState } from "react";
-import { useSelector } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
-import ArtistEvent from "../cards/ArtistEvent.card";
-import Pagination from "../Pagination.component";
+import React from "react";
+
 import DefaultErrorMessage from "../errors/DefaultErrorMessage.error";
 import styles from "./ArtistEvents.list.module.css";
 
-const ArtistEventsList = ({
-  apiStatus,
-  apiErrorMessage,
-  query,
-  filterStatus,
-}) => {
-  let artistEventsData = useSelector((state) => state.aes);
-  let aeObjs = useMemo(
-    () => artistEventsData?.artistEvents,
-    [artistEventsData.artistEvents]
-  );
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(6);
-
-  let cards = aeObjs.map((ae) => <ArtistEvent key={nanoid()} ae={ae} />);
-
-  let sortedCards = cards.sort((a, b) =>
-    Object.keys(a.props.ae) > Object.keys(b.props.ae) ? 1 : -1
-  );
-
-  const cardCount = sortedCards.length;
-
-  const totalPages = Math.ceil(cardCount / itemsPerPage);
-
-  // Filter logic
-  const filteredCards = filterStatus
-    ? sortedCards.filter(({ props }) => {
-        return Object.keys(props.ae)[0]
-          .toLowerCase()
-          .includes(query.toLowerCase());
-      })
-    : sortedCards;
-
-  // Pagination logic
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const paginatedCards = filteredCards.slice(startIndex, endIndex);
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
-
-  const handleItemsPerPageChange = (ipp) => {
-    setItemsPerPage(ipp);
-    setCurrentPage(1);
-  };
-
-  return (
-    <div className="container-md">
-      {apiStatus === "failed" && (
-        <DefaultErrorMessage error={apiErrorMessage} />
-      )}
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        itemCount={cardCount}
-        onPageChange={handlePageChange}
-        onItemsPerPageChange={handleItemsPerPageChange}
-      />
+const ArtistEventsList = ({ apiStatus, apiErrorMessage, paginatedCards }) => (
+  <div className="container-md">
+    {apiStatus === "failed" ? (
+      <DefaultErrorMessage error={apiErrorMessage} />
+    ) : (
       <div className={`d-flex flex-wrap justify-content-center ${styles.grid}`}>
         {paginatedCards}
       </div>
-    </div>
-  );
-};
+    )}
+  </div>
+);
 
 export default ArtistEventsList;
