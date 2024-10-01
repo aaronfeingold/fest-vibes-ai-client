4fc66c6759969f14d786b220da379d9275caa245
aaronfeingold <ajfeingold88@gmail.com>
2024-10-01 17:45:41 -0400

    testing failure...

--- src/services/ArtistEvents.service.js
+++ src/services/ArtistEvents.service.js
@@ -7,14 +7,14 @@ export const fetchArtistEvents = createAsyncThunk(
   async () => {
     if (process.env.REACT_APP_DEV_ENV === "development") {
       try {
-        const response = await fetch("/data.json"); // Fetch the local JSON file
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const json = await response.json(); // Parse the response body as JSON
-        return json.data;
+        // const response = await fetch("/data.json"); // Fetch the local JSON file
+        // if (!response.ok) {
+        //   throw new Error("Network response was not ok");
+        // }
+        // const json = await response.json(); // Parse the response body as JSON
+        // return json.data;
+        throw new Error("Forced API error");
       } catch (error) {
-        console.error("Error fetching local data.json:", error);
         throw error;
       }
     }
