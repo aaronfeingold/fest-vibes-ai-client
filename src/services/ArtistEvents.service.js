5bb742265f5a781ca36ab216651b20641933c654
aaronfeingold <ajfeingold88@gmail.com>
2024-10-03 17:17:34 -0400

    remove development stuff

--- src/services/ArtistEvents.service.js
+++ src/services/ArtistEvents.service.js
@@ -5,22 +5,9 @@ export const fetchArtistEvents = createAsyncThunk(
   "aes/fetchArtistEvents",
   // get req to AWS API gateway, calls Lambda scrapper python function
   async () => {
-    if (process.env.REACT_APP_DEV_ENV === "development") {
-      try {
-        // const response = await fetch("/data.json"); // Fetch the local JSON file
-        // if (!response.ok) {
-        //   throw new Error("Network response was not ok");
-        // }
-        // const json = await response.json(); // Parse the response body as JSON
-        // return json.data;
-        throw new Error("Forced API error");
-      } catch (error) {
-        throw error;
-      }
-    }
     try {
       const response = await axiosAPI.get("/");
-      return response.data.data;
+      return response.data.body.data;
     } catch (error) {
       console.error("Error fetching from API:", error);
       throw error;
