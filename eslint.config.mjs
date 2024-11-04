2885b7f68101bb81a941bfbbae9505036a98561a
aaron.feingold <ajfeingold88@gmail.com>
2024-11-04 17:02:37 -0600

    i just want to lint...should be so simple.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
+    files: ['src/**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
