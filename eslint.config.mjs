3d450b71c697e3c654be578f8dc2dff14ccfb1a7
aaron.feingold <ajfeingold88@gmail.com>
2024-11-04 16:14:06 -0600

    modify lint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,25 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: true, // Define process as a global variable
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      // Add any specific rules here
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
