3fb017d877879ccd130d7ad4d2fbe9157a264e60
aaron.feingold <ajfeingold88@gmail.com>
2025-01-15 17:12:51 -0600

    a fine looking error message i guess....

--- src/components/errors/DefaultErrorMessage.js
+++ src/components/errors/DefaultErrorMessage.js
@@ -41,38 +41,33 @@ const DefaultErrorMessage = ({ error }) => {
   };
 
   return (
-    <div className="container py-3 error-container rounded">
+    <div className="container py-4 error-container rounded">
       <div
         className={`alert alert-${alertClass} shadow-sm fade show`}
         role="alert"
       >
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-start">
           <div
             className="flex-shrink-0 me-3 fs-4 .error-icon"
             dangerouslySetInnerHTML={{ __html: getIcon(alertClass) }}
-          />
+          ></div>
           <div className="flex-grow-1">
-            <div className="d-flex align-items-center mb-1 .error-header">
+            <div className="d-flex justify-content-between mb-2">
               <h5 className="alert-heading mb-0">
                 {type.charAt(0).toUpperCase() + type.slice(1)}
               </h5>
-              {code && (
-                <span className="fs-4 badge bg-light text-dark border">
-                  Code: {code}
-                </span>
-              )}
+              {code && <span className="fs-6 text-dark">Code: {code}</span>}
             </div>
-            <p className="mb-0 fs-4">
-              {
-                "Unfortunately, today's events cannot be displayed at this time."
-              }
+            <p className="mb-0 fs-5">
+              Unfortunately, today's events cannot be displayed at this time.
             </p>
-            <p className="mb-0 fs-4 fw-bold text-danger">
-              Error Message {message}
+            <p className="mb-0 fs-5 fw-bold text-danger">
+              Error Message: {message}
             </p>
             {code >= 500 && (
-              <p className="mb-0 fs-6 ">
-                The wwoz.org livewire may be having technical difficulties...
+              <p className="mb-0 fs-6 text-muted">
+                The server may be experiencing technical difficulties. Please
+                try again later.
               </p>
             )}
           </div>
